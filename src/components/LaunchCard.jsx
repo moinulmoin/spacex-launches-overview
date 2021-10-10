1ecd12ecbb6996cfcda0038eda9535f51b8e2b96
Moinul Moin <moinulmoin101@gmail.com>
2021-10-10 17:03:26 +0600

    added picture and learn more link in launch card

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -2,6 +2,17 @@ function LaunchCard({ launch }) {
     return (
         <div className="col-12 col-sm-2 col-md-4 col-xl-3">
             <div className="card h-100">
+                {launch.links.mission_patch ? (
+                    <img
+                        className="card-img-top"
+                        src={launch.links.mission_patch}
+                        alt={launch.mission_name}
+                    />
+                ) : (
+                    <div className="card-img-top h-50 d-flex align-items-center justify-content-center">
+                        <h5 className="text-center">Sorry, picture is not available here</h5>
+                    </div>
+                )}
                 <div className="card-body">
                     <h6 className="card-subtitle mb-3">
                         <span className="text-muted">Year:</span> {launch.launch_year}
@@ -26,6 +37,9 @@ function LaunchCard({ launch }) {
                     <h5 className="card-title">
                         <span className="text-muted">Rocket type:</span> {launch.rocket.rocket_type}
                     </h5>
+                    <a href={launch.links.article_link} className="card-link">
+                        Learn More
+                    </a>
                 </div>
             </div>
         </div>
